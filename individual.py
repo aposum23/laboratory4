e40ee97d95c8722eaba87e3e80c2f42dae827f11
aposum23 <81620056+aposum23@users.noreply.github.com>
2021-10-30 23:31:11 +0300

    Delete individual.py

--- individual.py
+++
@@ -1,8 +0,0 @@
-import math
-
-
-osn_one = int(input('Osnovanie 1:'))
-osn_two = int(input('Osnovanie 2: '))
-angle = int(input('Ugol: '))
-
-print('Ploshad: ' + str(osn_one * osn_two * math.sin(angle)))
