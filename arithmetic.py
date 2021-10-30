24d3f31eba6498275e36d6f3fa43a0a17260963a
aposum23 <misha.duhno@mail.ru>
2021-10-30 23:33:04 +0300

    Deleting

--- arithmetic.py
+++
@@ -1,6 +0,0 @@
-print('4 * 100 - 54')
-user_num = input('Input answer: ')
-
-right_answ = 4 * 100 - 54
-
-print(f'Your answer: {user_num} \nRight answer: {right_answ}')
--- numbers.py
+++
@@ -1,6 +0,0 @@
-num_one = int(input('First number: '))
-num_two = int(input('Second number: '))
-num_three = int(input('Third number: '))
-num_four = int(input('Fourth number: '))
-
-print('Result: ' + str(format((num_one + num_two) / (num_three + num_four), '.2f')))
